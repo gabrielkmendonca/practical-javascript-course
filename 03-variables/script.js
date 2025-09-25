73d4b4eb3b7efba0dbc3223c670937aa65af92a0
Gabriel Karlinski Mendonça <172616949+gabrielkmendonca@users.noreply.github.com>
2025-09-25 17:15:45 -0400

    update

---
+++ 03-variables/script.js
@@ -0,0 +1,15 @@
+console.log(`JavaScript variables are like containers that keep data. They can be declared using 'let' or 'const'.`)
+
+let x1 = 5;
+let y1 = 6;
+let z1 = x1 + y1;
+/*  when declaring a variable using 'let', its value can be changed
+    the '=' symbols is the assignment operator, it assigns a value into a variable
+*/
+const x2 = 5;
+const y2 = 6;
+const z2 = x2 + y2;
+// declaring a variable using 'const' makes its value immutable
+
+console.log(`JavaScript variable are Case Sensitive, wich means that variables with lower and higher cases are different. For example, "lastname" and "lastName" are different variables.`)
+console.log(`Its not a rule, but historically the JavaScript variables are declared using the Lower Camel Case method. As an example, the variable for 'First Name' is declared as firstName`)
