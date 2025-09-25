a9b84b61369870cce1f5f55f57eb50e9de032a22
Gabriel Karlinski Mendonça <172616949+gabrielkmendonca@users.noreply.github.com>
2025-09-25 19:29:44 -0400

    Create script.js

---
+++ 05-operators/script.js
@@ -0,0 +1,60 @@
+console.log(`Now, lets take a look on some operations we can do using variables`)
+
+//Assigment (=) 
+const a = 5
+const b = 2
+
+//Addition (+)
+let ad = a + b
+
+//Subtraction (-)
+let sub = a - b
+
+//Multiplication (*)
+let mult = a * b
+
+//Exponentiation (*)*
+let exp = a**2
+
+//Division (/)
+let div = a / b
+
+//Division remainder (%)
+let rem = a % b
+
+//Increment
+let y = 5
+y++; //add 1 to the variable
+
+//Decrement
+let z = 5
+z--; //substract 1 to the variable
+
+//Concatenate (+) - (when working with strings)
+let txt1 = 'John'
+let txt2 = 'Doe'
+let concat = txt1 + '' + txt2 //concatenation joins strings
+
+//Assignment operators (assigns values while operation occurs at the same time)
+let x = 0
+x += a // adds value to the variable
+x -= a // substract value from the variable
+x *= a // multiplies the value to the variable
+x /= a // divides the variable by the value
+x %= a // gets the division remainder of the variable by the value
+x **= a // exponentiate the variable by the value
+
+//Comparison
+x == 5 //equal to
+x === 5 //equal value and equal type 
+x != 5 //not equal
+x !== 5 //not equal value and not equal type
+x > 5 //greater than
+x < 5 //less than
+x >= 5 //greater than or equal to
+x <= 5 //less than or equal to
+
+//Logical
+&& //logical and
+|| //logical or
+!  //logical not
