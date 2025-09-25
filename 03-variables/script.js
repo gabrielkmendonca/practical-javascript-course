76a012bf744186a0ff90fdb1bf8adbb0e8da61b1
Gabriel Karlinski Mendonça <172616949+gabrielkmendonca@users.noreply.github.com>
2025-09-25 18:34:16 -0400

    Update script.js

--- 03-variables/script.js
+++ 03-variables/script.js
@@ -11,6 +11,9 @@ const y2 = 6;
 const z2 = x2 + y2;
 // declaring a variable using 'const' makes its value immutable
 
+let name = window.prompt("Insert your name: ")
+//using variables with functions allow to get the value from inputs
+
 console.log(`JavaScript variable are Case Sensitive, wich means that variables with lower and higher cases are different. For example, "lastname" and "lastName" are different variables.`)
 console.log(`Its not a rule, but historically the JavaScript variables are declared using the Lower Camel Case method. As an example, the variable for 'First Name' is declared as firstName`)
 console.log(`The variables identifiers needs to follow some rules: can only start with letters, $ or _; cannot have spaces; and cannot be keywords`)
