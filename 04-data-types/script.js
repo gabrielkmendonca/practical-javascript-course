ccbb3bcc7b26ed583ff0a001a962e7eb358a4d19
Gabriel Karlinski Mendonça <172616949+gabrielkmendonca@users.noreply.github.com>
2025-09-27 00:09:20 -0400

    Update script.js

--- 04-data-types/script.js
+++ 04-data-types/script.js
@@ -1,8 +1,8 @@
 console.log(`When working with variables, they have different data types, wich allows different operations. We are now going to take a look on data types and what they are used for.`)
 
 let color = "yellow" //string = stores a string, its content needs to be declared inside "", '' or ``
-let length = 16 //integer(number) = stores integer numbers, either positive and negative
-let weight = 7.5 //float(number) = stores numbers with decimals 
+let length = 16 //non-decimal number = stores integer numbers, either positive and negative
+let weight = 7.5 //decimal number = stores numbers with decimals 
 let x = true //boolean = can have "true" or "false" value
 let y //undefined = undefined variable
 let z = null //null = stores null value
